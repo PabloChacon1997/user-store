e9a065e3b6e5abd9013eb69e57b86a9b88a612c8
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-01-15 20:37:38 -0500

    Init project and init mongodb

---
+++ src/data/mongo/mongo-database.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+interface Options {
+  mongoUrl: string;
+  dbName: string;
+}
+
+
+export class MongoDatbase {
+  static async coonect(options: Options) {
+    const { mongoUrl, dbName } = options;
+
+    try {
+      await mongoose.connect(mongoUrl, {
+        dbName,
+      });
+      return true;
+    } catch (error) {
+      console.log('Mongo connection error')
+      throw error;
+    }
+  }
+}

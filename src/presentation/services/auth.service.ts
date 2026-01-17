84c12446368fb2bf815339e34260c184d794df49
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-01-16 20:37:48 -0500

    login and resgister user

---
+++ src/presentation/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { bcrytpAdapter } from "../../config";
+import { UserModel } from "../../data";
+import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from "../../domain";
+
+
+
+export class AuthService {
+  // DI
+  constructor() {}
+
+  public async registerUser(registerUserDto: RegisterUserDto) {
+    const existUser = await UserModel.findOne({ email: registerUserDto.email });
+    if (existUser) throw CustomError.badRequest('Email already exist');
+
+    try {
+      const user = new UserModel(registerUserDto);
+      // Encriptar la contraseña
+      user.password = bcrytpAdapter.hash(registerUserDto.password);
+      await user.save();
+      // JWT para mantener la autenticación
+      // Email de confirmación
+
+      const {password, ...userEntity} = UserEntity.fromObject(user);
+      return {
+        user: userEntity,
+        token: 'ABC'
+      };
+    } catch (error) {
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+
+  public async loginUser(loginUserDto: LoginUserDto) {
+
+    const user = await UserModel.findOne({ email: loginUserDto.email });
+    if (!user) throw CustomError.badRequest('Email not exist');
+
+    const isMatching = bcrytpAdapter.compare(loginUserDto.password, user.password);
+    if (!isMatching) throw CustomError.badRequest('Password is not valid');
+    const {password, ...userEntity} = UserEntity.fromObject(user);
+    return {
+      user: userEntity,
+      token: 'ABC'
+    }
+  }
+}
